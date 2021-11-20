32d0475e83ab46aa8006e8f4d71ad25021f1fee7
Joseph Sebastian <josephsv96@gmail.com>
2021-11-20 11:09:11 +0100

    reapplied styles, tailwind working in prod env, inline tailwind not is purged

--- themes/windytheme/assets/css/postcss.config.js
+++ themes/windytheme/assets/css/postcss.config.js
@@ -1,13 +1,13 @@
-const themeDir = __dirname + '/../../';
+const themeDir = __dirname;
 
 module.exports = {    
     plugins: [   
         require('postcss-import')({
             path: [themeDir]
             }), 
-        require('tailwindcss')(themeDir + 'assets/css/tailwind.config.js'),
+        require('tailwindcss')(themeDir + '/tailwind.config.js'),
         require('autoprefixer')({
-            path: [themeDir]
-        }),
+            path:[themeDir]
+        })
     ]
 }
