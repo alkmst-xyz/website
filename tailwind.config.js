11046a37a10cb6396c518b58a69c801c0103dcba
Joseph Sebastian <josephsv96@gmail.com>
2022-10-05 22:19:36 +0200

    removed unused styles, components, consolidated css import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  content: ["./src/**/*.{html,njk}"],
+  content: ["./src/*.{html,njk}", "./src/**/*.{html,njk}"],
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [],
 };
