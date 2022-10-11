5087d80440b1858ef6e7b5c4b736a7c84757aec7
Joseph Sebastian <josephsv96@gmail.com>
2022-10-11 22:44:58 +0200

    added markdown footnotes, anchors, emoji support

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,96 @@
 const path = require("path");
 const { DateTime } = require("luxon");
+
+const markdownIt = require("markdown-it");
+const markdownItAnchor = require("markdown-it-anchor");
+const markdownItFootnote = require("markdown-it-footnote");
+const markdownItEmoji = require("markdown-it-emoji");
+const markdownItTexmath = require("markdown-it-texmath");
 const htmlmin = require("html-minifier");
+
 const Image = require("@11ty/eleventy-img");
 const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
+
+module.exports = function (eleventyConfig) {
+  // plugins
+  eleventyConfig.addPlugin(pluginSyntaxHighlight);
+  eleventyConfig.addPlugin(pluginNavigation);
+
+  // transform: minify HTML
+  eleventyConfig.addTransform("htmlmin", minifyHTML);
+
+  // shortcode: image
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
+
+  // filter: date
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
+      "dd LLL yyyy"
+    );
+  });
+
+  // filter: tags
+  function filterTagList(tags) {
+    return (tags || []).filter(
+      (tag) => ["all", "nav", "post", "posts"].indexOf(tag) === -1
+    );
+  }
+  eleventyConfig.addFilter("filterTagList", filterTagList);
+
+  // filter: date string
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
+  });
+
+  // force 11ty to watch CSS and JS files
+  eleventyConfig.addWatchTarget("./tailwind.config.js");
+  eleventyConfig.addWatchTarget("./src/css/styles.css");
+
+  // pass through static files
+  eleventyConfig.addPassthroughCopy("./src/static");
+
+  eleventyConfig.addGlobalData("generated", () => {
+    let now = new Date();
+    return new Intl.DateTimeFormat("en-US", {
+      dateStyle: "short",
+      timeStyle: "long",
+    }).format(now);
+  });
+
+  // customize markdown library and settings:
+  let mdLib = markdownIt({
+    html: true,
+    linkify: true,
+  })
+    .use(markdownItAnchor, {
+      permalink: markdownItAnchor.permalink.ariaHidden({
+        placement: "after",
+        class: "direct-link",
+        symbol: "∮",
+      }),
+      level: [1, 2, 3, 4],
+      slugify: eleventyConfig.getFilter("slugify"),
+    })
+    .use(markdownItFootnote)
+    .use(markdownItEmoji)
+    .use(markdownItTexmath, {
+      engine: require("katex"),
+      delimiters: "dollars",
+    });
+  eleventyConfig.setLibrary("md", mdLib);
+
+  return {
+    templateFormats: ["md", "njk"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dir: {
+      input: "src",
+      output: "_site",
+    },
+  };
+};
 
 async function imageShortcode(src, alt, sizes) {
   console.log(`Generating image(s) from:  ${src}`);
@@ -31,70 +118,18 @@ async function imageShortcode(src, alt, sizes) {
   });
 }
 
-module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(pluginSyntaxHighlight);
-  eleventyConfig.addPlugin(pluginNavigation);
-
-  // Shortcodes
-
-  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
-  eleventyConfig.addLiquidShortcode("image", imageShortcode);
-  eleventyConfig.addJavaScriptFunction("image", imageShortcode);
-
-  // Filters
-
-  // date
-  eleventyConfig.addFilter("readableDate", (dateObj) => {
-    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
-      "dd LLL yyyy"
-    );
-  });
-
-  // tags
-  function filterTagList(tags) {
-    return (tags || []).filter(
-      (tag) => ["all", "nav", "post", "posts"].indexOf(tag) === -1
-    );
-  }
-  eleventyConfig.addFilter("filterTagList", filterTagList);
-
-  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
-    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
-  });
-
-  // Others
-
-  eleventyConfig.addWatchTarget("./tailwind.config.js");
-  eleventyConfig.addWatchTarget("./src/css/styles.css");
-  eleventyConfig.addPassthroughCopy("./src/static");
-
-  eleventyConfig.addTransform("htmlmin", (content, outputPath) => {
-    if (outputPath.endsWith(".html")) {
-      const minified = htmlmin.minify(content, {
-        useShortDoctype: true,
-        removeComments: true,
+function minifyHTML(content, outputPath) {
+  return outputPath.endsWith(".html")
+    ? htmlmin.minify(content, {
+        collapseBooleanAttributes: true,
         collapseWhitespace: true,
+        conservativeCollapse: true,
+        minifyCSS: true,
         minifyJS: true,
-      });
-      return minified;
-    }
-
-    return content;
-  });
-
-  eleventyConfig.addGlobalData("generated", () => {
-    let now = new Date();
-    return new Intl.DateTimeFormat("en-US", {
-      dateStyle: "short",
-      timeStyle: "long",
-    }).format(now);
-  });
-
-  return {
-    htmlTemplateEngine: "njk",
-    dir: {
-      input: "src",
-    },
-  };
-};
+        removeComments: true,
+        sortAttributes: true,
+        sortClassName: true,
+        useShortDoctype: true,
+      })
+    : content;
+}
--- src/posts/0-post.md
+++ src/posts/0-post.md
@@ -137,12 +137,14 @@ You can also use words, to fit your writing style more closely[^note].
 
 ## Writing mathematical expressions
 
-- This sentence uses `$` delimiters to show math inline: $\sqrt{3x-1}+(1+x)^2$
-- This expression uses `\$` to display a dollar sign: $\sqrt{\$4}$
-- To split <span>$</span>100 in half, we calculate $100/2$
+- This sentence uses `$` delimiters to show math inline: $\sqrt{3x-1}+(1+x)^2$.
+- This expression uses `\$` to display a dollar sign: $\sqrt{\$4}$.
+- Use `\$` to use `$` sign directly, for example: \$100.
 
 ### Writing expressions as blocks
 
 **The Cauchy-Schwarz Inequality**
 
-$$\left( \sum_{k=1}^n a_k b_k \right)^2 \leq \left( \sum_{k=1}^n a_k^2 \right) \left( \sum_{k=1}^n b_k^2 \right)$$
+$$
+\left( \sum_{k=1}^n a_k b_k \right)^2 \leq \left( \sum_{k=1}^n a_k^2 \right) \left( \sum_{k=1}^n b_k^2 \right)
+$$
