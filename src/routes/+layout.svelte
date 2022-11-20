7771e5c190bc10123e4a77199d0b2344b4e16c9a
Joseph Sebastian <josephsv96@gmail.com>
2022-11-20 17:28:48 +0100

    moved to sveltekit project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	// import Header from './Header.svelte';
+	import '$lib/css/styles.css';
+</script>
+
+<div class="">
+	<!-- <Header /> -->
+
+	<main>
+		<slot />
+	</main>
+
+	<!-- <footer>
+		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+	</footer> -->
+</div>
