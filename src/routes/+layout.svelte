cdd8897e3f956d1f4d050803497e4cfe4c718bec
Joseph Sebastian <josephsv96@gmail.com>
2022-12-08 08:57:50 +0100

    moved content files(#15)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script>
-	import Header from '$lib/components/Header.svelte';
-	import Footer from '$lib/components/Footer.svelte';
+	import Header from '../components/Header.svelte';
+	import Footer from '../components/Footer.svelte';
 	import '$lib/css/styles.css';
 </script>
 
