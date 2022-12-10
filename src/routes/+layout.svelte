c786e747ae5d2424e2934384ebdb5508f87d743d
Joseph Sebastian <josephsv96@gmail.com>
2022-12-10 16:01:19 +0100

    added page transitions (#15)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,31 @@
 <script>
+	import { fade } from 'svelte/transition';
+
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 	import '$lib/styles/styles.css';
 	import 'uno.css';
+
+	export let data;
+	const { currentRoute } = data;
 </script>
 
 <div
-	class="bg-slate-100 text-lightParagraph
+	class="
+  bg-slate-100 text-lightParagraph
   leading-relaxed tracking-normal subpixel-antialiased
   font-caskaydia_cove
-  selection:bg-lightSecondary"
+  selection:bg-lightSecondary
+  "
 >
 	<body class="max-w-screen-md mx-auto px-2">
 		<Header />
 
-		<main class="px-2">
-			<slot />
-		</main>
+		{#key currentRoute}
+			<main class="px-2" in:fade={{ duration: 150, delay: 150 }} out:fade={{ duration: 150 }}>
+				<slot />
+			</main>
+		{/key}
 
 		<Footer />
 	</body>
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,11 +1,18 @@
-import { dev } from '$app/environment';
-
 // SvelteKit loads some client-side JavaScript to help with navigation and preloading in the background
 // Set to false to remove disable this
 //
 // we don't need any JS on this page, though we'll load
 // it in dev so that we get hot module replacement
-export const csr = dev;
+// import { dev } from '$app/environment';
+// export const csr = dev;
 
 // all pages are prerendered
 export const prerender = true;
+
+export async function load({ url }) {
+	const currentRoute = url.pathname;
+
+	return {
+		currentRoute
+	};
+}
