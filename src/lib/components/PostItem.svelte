236ca28cb5d78c8779361952a1abb7f3993f3dc7
Joseph Sebastian <josephsv96@gmail.com>
2022-12-11 21:47:10 +0100

    added PostItem (#15)

---
+++ src/lib/components/PostItem.svelte
@@ -0,0 +1,16 @@
+<script>
+	export let url = '';
+	export let title = '';
+	export let date = '';
+</script>
+
+<div class="flex flex-col my-2">
+	<span class="text-2xl font-bold">
+		<a href={url}>
+			{title}
+		</a>
+	</span>
+	<span>
+		{date}
+	</span>
+</div>
