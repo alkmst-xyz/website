baf0d41f10ffe27671ee2790c718b637987cfb4f
Joseph Sebastian <josephsv96@gmail.com>
2022-12-27 19:11:43 +0530

    added imprint, updated link to logo in meta (#15)

---
+++ src/routes/imprint/+page.svelte
@@ -0,0 +1,21 @@
+<svelte:head>
+	<title>Imprint</title>
+	<meta name="description" content="Imprint" />
+</svelte:head>
+
+<h1>Imprint</h1>
+<div class="markdown">
+	<h3>Contact</h3>
+	<p>E-mail: josephsv96@gmail.com</p>
+
+	<h3>Disclaimer</h3>
+	<p>The information on this website represents my personal opinion.</p>
+	<p>This website is made with Sveltekit, hosted on Netlify and open-sourced on GitHub.</p>
+	<p>This website doesn't use cookies.</p>
+	<h3>External Links</h3>
+	<p>
+		Some pages contain links to other websites that are checked at the moment of writing. I have no
+		control of the contents of these sites. Due to the very nature of Internet they may change at
+		any time and I am under no circumstances responsible for their contents.
+	</p>
+</div>
